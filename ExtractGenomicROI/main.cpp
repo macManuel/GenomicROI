e0c16d36d2095a35a35505a742d603b563b49625
macManuel <manuel@Mein-MBP.fritz.box>
2014-11-13 12:22:28 +0100

    Initial Commit

---
+++ ExtractGenomicROI/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ExtractGenomicROI
+//
+//  Created by Manuel Tuschen on 13.11.14.
+//  Copyright (c) 2014 Manuel Tuschen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
